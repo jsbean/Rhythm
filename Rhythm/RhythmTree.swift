07eeac4910a7ba0cdc7b61a9690851de4b67c3df
James <jbean46@gmail.com>
2017-02-01 20:19:11 -0500

    Initial implementation of inserting rhythm trees

--- Rhythm/RhythmTree.swift
+++ Rhythm/RhythmTree.swift
@@ -19,9 +19,92 @@ public enum RhythmTree {
     
     indirect case branch(MetricalDuration, [RhythmTree])
     
-    case leaf(MetricalDuration)
+    case leaf(MetricalLeaf<Int>)
     
     public init(_ metricalDuration: MetricalDuration, _ trees: [RhythmTree] = []) {
         self = .branch(metricalDuration, trees)
+    }
+    
+    public init(_ metricalDuration: MetricalDuration, _ relativeDurations: [Int]) {
+
+        let leaves = relativeDurations
+            .lazy
+            .map { MetricalDuration($0, metricalDuration.denominator) }
+            .map { MetricalLeaf($0, MetricalContext.instance(.event(1))) }
+            .map { RhythmTree.leaf($0) }
+        
+        self = RhythmTree.branch(metricalDuration, Array(leaves))
+    }
+    
+    public var leaves: [MetricalLeaf<Int>] {
+        func flattened(accum: [MetricalLeaf<Int>], tree: RhythmTree) -> [MetricalLeaf<Int>] {
+            switch tree {
+            case .branch(_, let trees):
+                return trees.reduce(accum, flattened)
+            case .leaf(let value):
+                return accum + [value]
+            }
+        }
+        
+        return flattened(accum: [], tree: self)
+    }
+    
+    public func inserting(tree: RhythmTree, at index: Int) -> RhythmTree {
+
+        switch self {
+        case .leaf(let value):
+            return .branch(value.metricalDuration, [tree])
+        case .branch(let metricalDuration, let trees):
+            
+            guard let (left, right) = trees.split(at: index) else {
+                fatalError("Index out of range")
+            }
+
+            return .branch(metricalDuration, left + tree + right)
+        }
+    }
+}
+
+extension RhythmTree: CustomStringConvertible {
+    
+    /// Printed description.
+    public var description: String {
+        
+        func indents(_ amount: Int, value: String = "  ") -> String {
+            return (0 ..< amount).reduce("") { accum, _ in accum + value }
+        }
+        
+        func traverse(tree: RhythmTree, indentation: Int = 0) -> String {
+            
+            switch tree {
+            case .leaf(let value):
+                return indents(indentation) + "\(value)"
+            case .branch(let value, let trees):
+                return (
+                    indents(indentation) + "\(value)\n" +
+                        trees
+                            .map { traverse(tree: $0, indentation: indentation + 1) }
+                            .joined(separator: "\n")
+                )
+            }
+        }
+        
+        return traverse(tree: self)
+    }
+}
+
+// TODO: Move down to `Collections`, abstract to a `Sequence` extension
+extension Array {
+    
+    public func split(at index: Index) -> ([Element], [Element])? {
+        
+        guard index >= startIndex && index <= endIndex else {
+            return nil
+        }
+        
+        let left = Array(self[startIndex ..< index])
+        let right = index == endIndex ? [] : Array(self[index ..< endIndex])
+        
+        return (left, right)
     }
 }
--- RhythmTests/RhythmTreeTests.swift
+++ RhythmTests/RhythmTreeTests.swift
@@ -12,6 +12,119 @@ import Rhythm
 
 class RhythmTreeTests: XCTestCase {
 
+    func testInit() {
+        _ = RhythmTree(MetricalDuration(1,8))
+    }
+    
+    func testSingleLeaf() {
+        let rt = RhythmTree(MetricalDuration(1,8), [1])
+        XCTAssertEqual(rt.leaves.count, 1)
+    }
+    
+    func testMultipleLeaves() {
+        let rt = RhythmTree(MetricalDuration(1,8), [1,2,3,4])
+        XCTAssertEqual(rt.leaves.count, 4)
+    }
+    
+    func testInsertLeafInLeaf() {
+        let duration = MetricalDuration(1,8)
+        let context = MetricalContext<Int>.instance(.event(1))
+        let rt = RhythmTree.leaf(MetricalValue(duration, context))
+        
+        let leafToInsert = rt
+        let newRT = rt.inserting(tree: leafToInsert, at: 0)
+        XCTAssertEqual(newRT.leaves.count, 1)
+    }
+    
+    func testInsertLeafAtBeginningSingleDepthBranch() {
+        
+        // Original `RhythmTree`
+        let rt = RhythmTree(MetricalDuration(1,8), [1,2,3])
+        
+        print("original: \(rt)")
+        
+        // Prepare leaf to insert into original
+        let duration = MetricalDuration(0,8)
+        let context = MetricalContext<Int>.instance(.event(1))
+        let leafToInsert = RhythmTree.leaf(MetricalValue(duration, context))
+        
+        let newRT = rt.inserting(tree: leafToInsert, at: 0)
+        
+        print("new: \(newRT)")
+        
+        XCTAssertEqual(newRT.leaves.count, 4)
+    }
+    
+    func testInsertLeafInMiddleSingleDepthBranch() {
+        
+        // Original `RhythmTree`
+        let rt = RhythmTree(MetricalDuration(1,8), [1,3,4])
+        
+        print("original: \(rt)")
+        
+        // Prepare leaf to insert into original
+        let duration = MetricalDuration(2,8)
+        let context = MetricalContext<Int>.instance(.event(1))
+        let leafToInsert = RhythmTree.leaf(MetricalValue(duration, context))
+        
+        let newRT = rt.inserting(tree: leafToInsert, at: 1)
+        
+        print("new: \(newRT)")
+        XCTAssertEqual(newRT.leaves.count, 4)
+    }
+    
+    func testInsertLeafAtEndSingleDepthBranch() {
+        
+        // Original `RhythmTree`
+        let rt = RhythmTree(MetricalDuration(1,8), [1,2,3])
+        
+        print("original: \(rt)")
+        
+        // Prepare leaf to insert into original
+        let duration = MetricalDuration(4,8)
+        let context = MetricalContext<Int>.instance(.event(1))
+        let leafToInsert = RhythmTree.leaf(MetricalValue(duration, context))
+        
+        let newRT = rt.inserting(tree: leafToInsert, at: 3)
+        
+        print("new: \(newRT)")
+        XCTAssertEqual(newRT.leaves.count, 4)
+    }
+    
+    func testInsertBranchAtBeginningSingleDepthBranch() {
+        
+        // Original `RhythmTree`
+        let rt = RhythmTree(MetricalDuration(1,8), [3,4])
+        
+        print("original: \(rt)")
+        
+        // Prepare branch to insert into original
+        let duration = MetricalDuration(4,8)
+        let branchToInsert = RhythmTree(duration, [1,2])
+
+        let newRT = rt.inserting(tree: branchToInsert, at: 0)
+        
+        print("new: \(newRT)")
+        XCTAssertEqual(newRT.leaves.count, 4)
+    }
+    
+    func testInsertBranchInMiddleSingleDepthBranch() {
+        
+        // Original `RhythmTree`
+        let rt = RhythmTree(MetricalDuration(1,8), [1,4])
+        
+        print("original: \(rt)")
+        
+        // Prepare branch to insert into original
+        let duration = MetricalDuration(4,8)
+        let branchToInsert = RhythmTree(duration, [2,3])
+        
+        let newRT = rt.inserting(tree: branchToInsert, at: 1)
+        
+        print("new: \(newRT)")
+        XCTAssertEqual(newRT.leaves.count, 4)
+    }
+    
 //    func testLeaf() {
 //        let duration = MetricalDuration(1,8)
 //        let context = MetricalContext<Int>.instance(.event(1))
@@ -54,4 +167,26 @@ class RhythmTreeTests: XCTestCase {
 //        let tree = RhythmTree(MetricalDuration(4,8), leaves)
 //        print("Rhythm Tree: \(tree)")
 //    }
+    
+    //
+    func testSplitAtBeginning() {
+        let array = [1,2,3,4,5]
+        let (left, right) = array.split(at: 0)!
+        XCTAssertEqual(left, [])
+        XCTAssertEqual(right, [1,2,3,4,5])
+    }
+    
+    func testSplitInMiddle() {
+        let array = [1,2,3,4,5]
+        let (left, right) = array.split(at: 2)!
+        XCTAssertEqual(left, [1,2])
+        XCTAssertEqual(right, [3,4,5])
+    }
+    
+    func testSplitAtEnd() {
+        let array = [1,2,3,4,5]
+        let (left, right) = array.split(at: 5)!
+        XCTAssertEqual(left, [1,2,3,4,5])
+        XCTAssertEqual(right, [])
+    }
 }
