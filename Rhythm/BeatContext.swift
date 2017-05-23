0af5200c3d164bcb7ed9990f7df16bad7c96bb02
James <jbean46@gmail.com>
2017-05-23 15:03:36 -0400

    Add public initializer to BeatContext

--- Rhythm/BeatContext.swift
+++ Rhythm/BeatContext.swift
@@ -8,14 +8,27 @@
 
 /// Information about a given beat within a `Meter`.
 public struct BeatContext {
+
+    // MARK: - Associated Types
+    
+    /// Type defining the placement of a beat in metered context
+    public typealias Position = Int
+    
+    // MARK: - Instance Properties
+
+    /// Subdivision of `BeatContext`.
+    public let subdivision: Subdivision
     
     /// Offset in amount of beats at given subdivision level from downbeat.
     ///
     /// - Note: Downbeat = 0
-    public let position: Int
+    public let position: Position
 
-    /// Subdivision of `BeatContext`.
-    public let subdivision: Int
+    // MARK: - Initializers
     
-
+    /// Creates a `BeatContext` with the given `subdivision` and `position`.
+    public init(subdivision: Subdivision, position: Position) {
+        self.subdivision = subdivision
+        self.position = position
+    }
 }
