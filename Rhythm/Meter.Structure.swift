a48fc697215ae396291c460dd18914a52860b24c
James <jbean46@gmail.com>
2017-05-27 11:10:27 -0400

    Add doc comments

--- Rhythm/Meter.Structure.swift
+++ Rhythm/Meter.Structure.swift
@@ -14,7 +14,10 @@ extension Meter {
     /// Model of a metrical structure. Combines meters with tempo interpolation information.
     public struct Structure {
         
-        /// - returns: Array of `MetricalDuration` values
+        // MARK: - Instance Properties
+        
+        /// - returns: Array of `MetricalDuration` values of offset of each meter contained 
+        /// herein.
         public var meterOffsets: [MetricalDuration] {
             var result: [MetricalDuration] = []
             var accum: MetricalDuration = .zero
@@ -25,9 +28,15 @@ extension Meter {
             return result
         }
         
+        /// `Meter` values contained herein.
         public let meters: [Meter]
+        
+        /// `Tempo.Stratum` value contained herein.
         public let tempi: Tempo.Stratum
         
+        // MARK: - Initializers
+        
+        /// Creates a `Meter.Structure` with the given `meters` and `tempi`.
         public init(meters: [Meter] = [], tempi: Tempo.Stratum = Tempo.Stratum()) {
             self.meters = meters
             self.tempi = tempi
