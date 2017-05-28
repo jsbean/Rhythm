8be9ee5d480cc86f514b241d1baf2ce0614113bb
James <jbean46@gmail.com>
2017-05-28 17:16:14 -0400

    Teach BeatContext about Meter

--- Rhythm/BeatContext.swift
+++ Rhythm/BeatContext.swift
@@ -11,6 +11,9 @@ public struct BeatContext {
     
     // MARK: - Instance Properties
     
+    /// Meter containing `BeatContext`.
+    public let meter: Meter
+    
     /// Metrical offset of beat within `Meter`.
     public let offset: MetricalDuration
 
@@ -20,7 +23,8 @@ public struct BeatContext {
     // MARK: - Initializers
     
     /// Creates a `BeatContext` with the given `subdivision` and `position`.
-    public init(offset: MetricalDuration, interpolation: Tempo.Interpolation) {
+    public init(meter: Meter, offset: MetricalDuration, interpolation: Tempo.Interpolation) {
+        self.meter = meter
         self.offset = offset
         self.tempoContext = Tempo.Context(interpolation: interpolation, metricalOffset: offset)
     }
