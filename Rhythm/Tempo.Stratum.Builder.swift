ed61f1d0fec222b765256640b6d02a102214dad8
James <jbean46@gmail.com>
2017-05-31 10:09:34 +0200

    Add doc comments to Tempo.Stratum.Builder

--- Rhythm/Tempo.Stratum.Builder.swift
+++ Rhythm/Tempo.Stratum.Builder.swift
@@ -11,12 +11,21 @@ import ArithmeticTools
 
 extension Tempo.Stratum {
     
+    /// Builder for a `Tempo.Stratum`.
     public class Builder {
 
+        // The tempo information compiled through the build process.
         private var tempi: SortedDictionary<MetricalDuration, (Tempo, Bool)> = [:]
 
+        // MARK: - Initializers
+        
+        /// Creates a `Tempo.Stratum.Builder` prepared to build a `Tempo.Stratum`.
         public init() { }
         
+        // MARK: - Instance Properties
+        
+        /// Add the given `tempo` at the given `offset`, and whether or not it shall be 
+        /// prepared to interpolate to the next given tempo, or not.
         public func add(
             _ tempo: Tempo,
             at offset: MetricalDuration,
@@ -26,12 +35,18 @@ extension Tempo.Stratum {
             tempi[offset] = (tempo, interpolating)
         }
         
+        /// - Returns: `Tempo.Stratum` value with the `tempo` values injected through the use
+        /// of the `add(:at:interpolating)` method.
         public func build() -> Tempo.Stratum {
             
+            // If no tempi have been added, return a `Tempo.Stratum` value with a single
+            // value of `Tempo(60)` at an offset of `.zero`.
             guard !tempi.isEmpty else {
                 return Tempo.Stratum()
             }
             
+            // If only a single tempo has been added, create a `Tempo.Stratum` with a single
+            // value of the tempo added, at an offset of `.zero`.
             guard tempi.count > 1 else {
                 let (_ ,(tempo, _)) = tempi[0]
                 return Tempo.Stratum(tempi: [.zero: Tempo.Interpolation(tempo: tempo)])
