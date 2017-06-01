39bfa39e57292eaf06069c391da5465b3dfc8107
Brian Heim <brianlheim@gmail.com>
2017-05-31 21:09:43 -0400

    Add exponentialInOut, sineInOut tests

--- RhythmTests/EasingTests.swift
+++ RhythmTests/EasingTests.swift
@@ -11,6 +11,8 @@ import XCTest
 
 class EasingTests: XCTestCase {
 
+    //****** General errors *******//
+
     func testEasingErrorLessThanZero() {
 
         let x: Double = -0.5
@@ -27,6 +29,8 @@ class EasingTests: XCTestCase {
         XCTAssertThrowsError(try ease.evaluate(at: x))
     }
 
+    //****** linear *******//
+
     func testLinear() {
 
         let x: Double = 0.5
@@ -40,6 +44,8 @@ class EasingTests: XCTestCase {
             XCTFail()
         }
     }
+
+    //****** exponentialIn *******//
 
     func testExponentialInOne() {
 
@@ -83,7 +89,6 @@ class EasingTests: XCTestCase {
         }
     }
 
-
     func testExponentialInError() {
 
         let x: Double = 0.5
@@ -92,5 +97,61 @@ class EasingTests: XCTestCase {
         XCTAssertThrowsError(try ease.evaluate(at: x))
     }
 
+    //****** exponentialInOut *******//
+
+    func testExponentialInOutOne() {
+
+        let x: Double = 0.25
+        let ease = Interpolation.Easing.exponentialInOut(exponent: 1)
+        let expected = 0.25
+
+        do {
+            let result = try ease.evaluate(at: x)
+            XCTAssertEqual(result, expected)
+        } catch {
+            XCTFail()
+        }
+    }
+
+    func testExponentialInOutTwo() {
+
+        let ease = Interpolation.Easing.exponentialInOut(exponent: 2)
+
+        XCTAssertEqual(try ease.evaluate(at: 0), 0)
+        XCTAssertEqual(try ease.evaluate(at: 0.25), 0.125)
+        XCTAssertEqual(try ease.evaluate(at: 0.5), 0.5)
+        XCTAssertEqual(try ease.evaluate(at: 0.75), 0.875)
+        XCTAssertEqual(try ease.evaluate(at: 1), 1)
+    }
+
+    func testExponentialInOutThree() {
+
+        let ease = Interpolation.Easing.exponentialInOut(exponent: 3)
+
+        XCTAssertEqual(try ease.evaluate(at: 0), 0)
+        XCTAssertEqual(try ease.evaluate(at: 0.25), 0.0625)
+        XCTAssertEqual(try ease.evaluate(at: 0.5), 0.5)
+        XCTAssertEqual(try ease.evaluate(at: 0.75), 0.9375)
+        XCTAssertEqual(try ease.evaluate(at: 1), 1)
+    }
+
+
+    func testExponentialInOutError() {
+        XCTAssertThrowsError(try Interpolation.Easing.exponentialInOut(exponent: -2).evaluate(at: 0))
+        XCTAssertThrowsError(try Interpolation.Easing.exponentialInOut(exponent: 0.5).evaluate(at: 0))
+    }
+
+    //****** sineInOut *******//
+
+    func testSineInOut() {
+
+        let ease = Interpolation.Easing.sineInOut
+
+        XCTAssertEqual(try ease.evaluate(at: 0), 0)
+        XCTAssertEqualWithAccuracy(try ease.evaluate(at: 0.25), (1 - 1/sqrt(2)) / 2, accuracy: 1e-12)
+        XCTAssertEqualWithAccuracy(try ease.evaluate(at: 0.5), 0.5, accuracy: 1e-12)
+        XCTAssertEqualWithAccuracy(try ease.evaluate(at: 0.75), (1 + 1/sqrt(2)) / 2, accuracy: 1e-12)
+        XCTAssertEqual(try ease.evaluate(at: 1), 1)
+    }
 
 }
