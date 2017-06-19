0efd50b9a76079d0b908bf468860acc4fbdba6c1
Brian Heim <brianlheim@gmail.com>
2017-06-19 11:18:36 -0400

    Add Interpolation.tempo tests

--- RhythmTests/InterpolationTests.swift
+++ RhythmTests/InterpolationTests.swift
@@ -13,5 +13,37 @@ import ArithmeticTools
 
 class InterpolationTests: XCTestCase {
 
+    func testTempoLinear() {
+
+        let interp = Interpolation(
+            start: Tempo(60),
+            end: Tempo(120),
+            duration: 1 /> 1,
+            easing: Interpolation.Easing.linear)
+
+        XCTAssertEqual(interp.tempo(at: 0 /> 1), Tempo(60))
+        XCTAssertEqualWithAccuracy(
+            interp.tempo(at: 1 /> 2).beatsPerMinute,
+            Tempo(84.85).beatsPerMinute,
+            accuracy: 0.01)
+        XCTAssertEqual(interp.tempo(at: 1 /> 1), Tempo(120))
+    }
+
+    func testTempoPowerIn() {
+
+        let interp = Interpolation(
+            start: Tempo(60),
+            end: Tempo(120),
+            duration: 1 /> 1,
+            easing: Interpolation.Easing.powerIn(exponent: 2))
+
+        XCTAssertEqual(interp.tempo(at: 0 /> 1), Tempo(60))
+        XCTAssertEqualWithAccuracy(
+            interp.tempo(at: 1 /> 2).beatsPerMinute,
+            Tempo(71.35).beatsPerMinute,
+            accuracy: 0.01)
+        XCTAssertEqual(interp.tempo(at: 1 /> 1), Tempo(120))
+    }
+
 
 }
