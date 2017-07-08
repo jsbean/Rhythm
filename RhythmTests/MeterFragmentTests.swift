268057fb7c3d9de07393b7ecd5a8ed4872039d24
James <jbean46@gmail.com>
2017-07-08 22:53:17 +0200

    Add test for Meter.Fragment range sanitizer

--- RhythmTests/MeterFragmentTests.swift
+++ RhythmTests/MeterFragmentTests.swift
@@ -7,11 +7,23 @@
 //
 
 import XCTest
+import ArithmeticTools
 import Rhythm
 
 class MeterFragmentTests: XCTestCase {
 
     func testCleanseRange() {
 
+        let meter = Meter(11,16)
+        let range = Fraction(4,32) ... Fraction(2,4)
+
+        let fragment = Meter.Fragment(meter, in: range)
+        XCTAssertEqual(fragment.range, range)
+
+        // Expected: 4/32 ... 16/32
+        XCTAssertEqual(fragment.range.lowerBound.numerator, 4)
+        XCTAssertEqual(fragment.range.lowerBound.denominator, 32)
+        XCTAssertEqual(fragment.range.upperBound.numerator, 16)
+        XCTAssertEqual(fragment.range.upperBound.denominator, 32)
     }
 }
