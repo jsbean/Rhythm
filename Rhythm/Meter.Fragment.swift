01d9515b520dad92ee4438e10b3f02fdb44e0146
James <jbean46@gmail.com>
2017-07-08 22:38:50 +0200

    Sanitize ranges in Meter.Fragment

--- Rhythm/Meter.Fragment.swift
+++ Rhythm/Meter.Fragment.swift
@@ -38,8 +38,8 @@ extension Meter {
         public let range: ClosedRange<Fraction>
 
         public init(_ meter: Meter, in range: ClosedRange<Fraction>) {
+            self.range = sanitized(range: range, for: meter)
             self.meter = meter
-            self.range = range
         }
 
         public init(
@@ -48,11 +48,35 @@ extension Meter {
             to end: Fraction? = nil
         )
         {
-            let end = end?.clamped(in: .unit ... Fraction(meter)) ?? Fraction(meter)
+            let end = end ?? Fraction(meter)
             let range = start ... end
             self.init(meter, in: range)
         }
     }
+}
+
+func sanitized(range: ClosedRange<Fraction>, for meter: Meter) -> ClosedRange<Fraction> {
+    let full = .unit ... Fraction(meter)
+    let range = range.lowerBound.clamped(in: full) ... range.upperBound.clamped(in: full)
+    return normalized(range: range, for: meter)
+}
+
+/// - Returns: Range which has a denominator >= to that of the given `meter`.
+func normalized(range: ClosedRange<Fraction>, for meter: Meter) -> ClosedRange<Fraction> {
+
+    // Ensure that the range is represented with the same denominator
+    // FIXME: Use fixed `Ration.respelling(denominator:)`
+    let common = lcm(range.lowerBound.denominator, range.upperBound.denominator)
+    let start = respellingDenominator(of: range.lowerBound, to: common)!
+    let end = respellingDenominator(of: range.upperBound, to: common)!
+
+    guard start.denominator >= meter.denominator else {
+        let start = respellingDenominator(of: start, to: meter.denominator)!
+        let end = respellingDenominator(of: end, to: meter.denominator)!
+        return start ... end
+    }
+
+    return start ... end
 }
 
 extension Meter.Fragment: Equatable {
@@ -60,4 +84,14 @@ extension Meter.Fragment: Equatable {
     public static func == (lhs: Meter.Fragment, rhs: Meter.Fragment) -> Bool {
         return lhs.meter == rhs.meter && lhs.range == rhs.range
     }
+}
+
+// FIXME: Temporary wrapper to cover up bug: https://github.com/dn-m/ArithmeticTools/issues/110
+func respellingDenominator <R: Rational> (of rational: R, to value: Int) -> R? {
+
+    if rational.numerator == 0 {
+        return R(0, value)
+    }
+
+    return rational.respelling(denominator: value)
 }
---
+++ RhythmTests/MeterFragmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeterFragmentTests.swift
+//  Rhythm
+//
+//  Created by James Bean on 7/8/17.
+//
+//
+
+import XCTest
+import Rhythm
+
+class MeterFragmentTests: XCTestCase {
+
+    func testCleanseRange() {
+
+    }
+}
