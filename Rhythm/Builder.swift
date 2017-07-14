ae5c683d3d226f2c56acd4dcdf24ae42672b987a
James <jbean46@gmail.com>
2017-07-14 10:40:38 +0100

    Abstract build() function upto SpanningContainerBuilder

--- Rhythm/Builder.swift
+++ Rhythm/Builder.swift
@@ -10,36 +10,35 @@ import Collections
 import ArithmeticTools
 
 protocol SpanningContainerBuilder: class {
-    associatedtype Product: MetricalDurationSpanningContainer
+    associatedtype Product: SpanningContainer
     typealias Element = Product.Element
-    var intermediate: SortedDictionary<Fraction,Element> { get set }
-    var offset: Fraction { get set }
-    func add(_: Element) -> Self
+    var intermediate: SortedDictionary<Element.Metric,Element> { get set }
+    //var offset: Fraction { get set }
+    //func add(_: Element) -> Self
 }
 
 extension SpanningContainerBuilder {
 
-    @discardableResult public func add(_ element: Element) -> Self {
-        self.intermediate.insert(element, key: offset)
-        offset += element.range.length
-        return self
-    }
+//    @discardableResult public func add(_ element: Element) -> Self {
+//        self.intermediate.insert(element, key: offset)
+//        offset += element.range.length
+//        return self
+//    }
 
-    @discardableResult public func add <S: Sequence> (_ elements: S) -> Self
-        where S.Iterator.Element == Element
-    {
-        elements.forEach { _ = add($0) }
-        return self
-    }
+//    @discardableResult public func add <S: Sequence> (_ elements: S) -> Self
+//        where S.Iterator.Element == Element
+//    {
+//        elements.forEach { _ = add($0) }
+//        return self
+//    }
 
     public func build() -> Product {
         return Product(intermediate)
     }
 }
 
-protocol MetricalDurationSpanningContainerBuilder: class {
+protocol MetricalDurationSpanningContainerBuilder: SpanningContainerBuilder {
     associatedtype Product: MetricalDurationSpanningContainer
-    typealias Element = Product.Element
     var intermediate: SortedDictionary<Fraction,Element> { get set }
     var offset: Fraction { get set }
     func add(_: Element) -> Self
@@ -58,10 +57,6 @@ extension MetricalDurationSpanningContainerBuilder {
     {
         elements.forEach { _ = add($0) }
         return self
-    }
-
-    public func build() -> Product {
-        return Product(intermediate)
     }
 }
 
