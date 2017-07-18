46c0758755d2338159f6181cd8c5423c673a1b3d
James <jbean46@gmail.com>
2017-07-18 19:55:43 +0100

    Add doc comments. Refine SpannerContainerBuilder API

--- Rhythm/SpanningContainerBuilder.swift
+++ Rhythm/SpanningContainerBuilder.swift
@@ -9,15 +9,34 @@
 import Collections
 import ArithmeticTools
 
+/// Interface for types which can build `SpanningContainer` types.
 public protocol SpanningContainerBuilder: class {
+
+    // MARK: - Associated Types
+
+    /// Type of product which is built by `SpanningContainerBuilder`.
     associatedtype Product: SpanningContainer
+
+    /// Spanner-type contained by the `Product`.
     typealias Spanner = Product.Spanner
+
+    // MARK: - Instance Properties
+
+    /// Intermediate storage which is converted into the `Product`.
     var intermediate: SortedDictionary<Spanner.Metric,Spanner> { get set }
+
+    // MARK: - Instance Methods
+
+    /// Adds the given `Spanner` to the `intermediate`.
     func add(_: Spanner) -> Self
+
+    /// Creates the final `Product`.
+    func build() -> Product
 }
 
 extension SpanningContainerBuilder {
 
+    /// Creates the final `Product` with the `intermediate`.
     public func build() -> Product {
         return Product(intermediate)
     }
