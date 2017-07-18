fa0ae73e53801542201f9cfd6edb997c088ab26e
James <jbean46@gmail.com>
2017-07-18 19:49:02 +0100

    Add doc comments, remove unnecessary Base constraint

--- Rhythm/SpanningFragment.swift
+++ Rhythm/SpanningFragment.swift
@@ -8,14 +8,19 @@
 
 import ArithmeticTools
 
+/// Interface extending `Spanning` types, which also carry with them a range of operation.
+// FIXME: Use constrained associated types (Metric == Fragment.Metric)
 public protocol SpanningFragment: Spanning, Fragmentable {
-    associatedtype Base: Fragmentable
-    var base: Base { get }
+
+    // MARK: - Instance Properties
+
+    /// The range of operation.
     var range: Range<Metric> { get }
 }
 
 extension SpanningFragment {
 
+    /// The length of `SpanningFragment`.
     public var length: Metric {
         return range.length
     }
@@ -23,12 +28,14 @@ extension SpanningFragment {
 
 extension SpanningFragment where Fragment == Self {
 
+    /// - Returns: A fragment of self from lower bound to the given `offset`.
     public func to(_ offset: Metric) -> Self {
         assert(offset <= self.range.upperBound)
         let range = self.range.lowerBound ..< offset
         return self[range]
     }
 
+    /// - Returns: A fragment of self from the given `offset` to upper bound.
     public func from(_ offset: Metric) -> Self {
         assert(offset >= self.range.lowerBound)
         let range = offset ..< self.range.upperBound
