2c1f14da9495ccab56fbb6c4f74d83568afc0bdb
James <jbean46@gmail.com>
2017-07-18 13:48:02 +0100

    Abstract up length, from(_:), and to(_:) to SpanningFragment

--- Rhythm/MetricalDurationSpanningFragment.swift
+++ Rhythm/MetricalDurationSpanningFragment.swift
@@ -8,30 +8,4 @@
 
 import ArithmeticTools
 
-public protocol MetricalDurationSpanningFragment: SpanningFragment, MetricalDurationSpanning {
-    associatedtype Base: Fragmentable
-    var base: Base { get }
-    var range: Range<Fraction> { get }
-}
-
-extension MetricalDurationSpanningFragment {
-
-    public var length: Fraction {
-        return range.length
-    }
-}
-
-extension MetricalDurationSpanningFragment where Fragment == Self {
-
-    func from(_ offset: Fraction) -> Self {
-        assert(offset >= self.range.lowerBound)
-        let range = offset ..< self.range.upperBound
-        return self[range]
-    }
-
-    func to(_ offset: Fraction) -> Self {
-        assert(offset <= self.range.upperBound)
-        let range = self.range.lowerBound ..< offset
-        return self[range]
-    }
-}
+public protocol MetricalDurationSpanningFragment: SpanningFragment, MetricalDurationSpanning { }
--- Rhythm/SpanningFragment.swift
+++ Rhythm/SpanningFragment.swift
@@ -6,4 +6,32 @@
 //
 //
 
-public protocol SpanningFragment: Spanning, Fragmentable { }
+import ArithmeticTools
+
+public protocol SpanningFragment: Spanning, Fragmentable {
+    associatedtype Base: Fragmentable
+    var base: Base { get }
+    var range: Range<Fraction> { get }
+}
+
+extension SpanningFragment {
+
+    public var length: Fraction {
+        return range.length
+    }
+}
+
+extension SpanningFragment where Fragment == Self {
+
+    func from(_ offset: Fraction) -> Self {
+        assert(offset >= self.range.lowerBound)
+        let range = offset ..< self.range.upperBound
+        return self[range]
+    }
+
+    func to(_ offset: Fraction) -> Self {
+        assert(offset <= self.range.upperBound)
+        let range = self.range.lowerBound ..< offset
+        return self[range]
+    }
+}
