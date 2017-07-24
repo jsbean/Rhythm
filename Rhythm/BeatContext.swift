518e065dcc925eee90e0e2000f33e77f7a2ae73c
James <jbean46@gmail.com>
2017-07-24 14:18:30 +0200

    Use Meter.Fragment as Element of Meter.Context

--- Rhythm/BeatContext.swift
+++ Rhythm/BeatContext.swift
@@ -16,32 +16,27 @@ public struct BeatContext {
 
     /// - returns: Metrical offset from start of a `Meter.Structure`.
     public var metricalOffset: MetricalDuration {
-        return meterContext.offset + beatOffset
+        let rangeOffsetFraction = meterContext.meter.range.lowerBound
+        let rangeOffset = rangeOffsetFraction.numerator /> rangeOffsetFraction.denominator
+        return meterContext.offset + offset - rangeOffset
     }
 
     /// Meter containing `BeatContext`.
     public let meterContext: Meter.Context
 
     /// Metrical offset of beat within `Meter`.
-    public let beatOffset: MetricalDuration
-
-    /// Context of tempo within `Tempo.Interpolation`.
-    public let tempoContext: Tempo.Context
+    public let offset: MetricalDuration
 
     // MARK: - Initializers
 
     /// Creates a `BeatContext` with the given `subdivision` and `position`.
     public init(
         meterContext: Meter.Context,
-        beatOffset: MetricalDuration,
+        offset: MetricalDuration,
         interpolation: Tempo.Interpolation
     )
     {
         self.meterContext = meterContext
-        self.beatOffset = beatOffset
-        self.tempoContext = Tempo.Context(
-            interpolation: interpolation,
-            metricalOffset: Fraction(meterContext.offset + beatOffset)
-        )
+        self.offset = offset
     }
 }
